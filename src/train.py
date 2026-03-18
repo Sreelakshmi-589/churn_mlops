bda07bf37771bfeb1356adae4e0fba55274fce8a
Sreelakshmi <ksreelakshmi589@gmail.com>
2026-03-18 21:58:15 +0530

    using pipeline instead of model + scaler

--- src/train.py
+++ src/train.py
@@ -1,48 +1,50 @@
+
 import pandas as pd #to handle data tables
 
-from sklearn.model_selection import train_test_split #splits data into training set and testing set
+from sklearn.model_selection import train_test_split #splits data into training set and test set
 from sklearn.preprocessing import StandardScaler #to normalise values: helps nn learn better
 from sklearn.neural_network import MLPClassifier #AI model
+from sklearn.pipeline import Pipeline 
 
 import os #manage folders
 import joblib #saves model
 
-os.makedirs("models", exist_ok=True) #creates models folder, if not present
+# Create models folder
+os.makedirs("models", exist_ok=True)
 
-data = pd.read_csv("data/churn.csv") #loads dataset
+# Load dataset
+data = pd.read_csv("data/churn.csv")
 
-#data cleaning
+# Data cleaning
 data["TotalCharges"] = pd.to_numeric(data["TotalCharges"], errors="coerce")
 data = data.dropna()
 
-#target conversion
+# Target conversion
 data["Churn"] = data["Churn"].map({"Yes": 1, "No": 0})
 
-#dropping unwanted columns
+# Drop unnecessary column
 data = data.drop("customerID", axis=1)
 
-#encoding categorical data
+# Encode categorical variables
 data = pd.get_dummies(data)
 
-#split features and target
+# Split features and target
 X = data.drop("Churn", axis=1)
 y = data["Churn"]
 
-#scaling
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+# Train-test split (IMPORTANT: no scaling before this now)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#train-test split
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+# Create pipeline (scaler + model)
+pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("model", MLPClassifier(hidden_layer_sizes=(64, 32), max_iter=200))
+])
 
-#model
-model = MLPClassifier(hidden_layer_sizes=(64, 32), max_iter=50)
+# Train pipeline
+pipeline.fit(X_train, y_train)
 
-#train
-model.fit(X_train, y_train)
+# Save pipeline
+joblib.dump(pipeline, "models/pipeline.pkl")
 
-#saving model
-joblib.dump(model, "models/model.pkl")
-joblib.dump(scaler, "models/scaler.pkl")
-
-print("Training completed")
+print("Pipeline training completed")
